20d56306371903cfa58f4a9fa48c150ab4556e1b
多多 <1092501244@qq.com>
2017-12-22 18:23:25 +0800

    更新我的界面

--- app/src/main/java/nongbang/hg/nongbang/SettingActivity.java
+++ app/src/main/java/nongbang/hg/nongbang/SettingActivity.java
@@ -18,6 +18,7 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        InitView();
     }
 
     public void InitView(){
